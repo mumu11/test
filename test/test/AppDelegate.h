46c701d01bdc0aaedcd518ca3ca0e35643ed989d
mu <amberstone6@163.com>
2019-06-05 11:07:55 +0800

    111

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by mu on 2019/6/5.
+//  Copyright © 2019 mu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by mu on 2019/6/5.
+//  Copyright © 2019 mu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
